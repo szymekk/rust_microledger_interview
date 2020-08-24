2f959fd8627a5cf2557e28aa1b70fb16f842385f
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2020-08-24 02:44:27 +0200

    Generate a random token string while pairing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
+type Token = String;
+
+fn generate_token() -> Token {
+    use rand::distributions::Alphanumeric;
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let token_string: Token = rng.sample_iter(&Alphanumeric).take(7).collect();
+    token_string
+}
+
 async fn handle_connection(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/pair") => {
-            let response = Response::new(Body::from("hello"));
+            let token_string = generate_token();
+            let response = Response::new(Body::from(token_string));
             Ok(response)
         }
 
