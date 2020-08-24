938bff1ade34c2bf238deb2cf62af333c57a09fd
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2020-08-24 03:46:54 +0200

    Listen for connections on a dynamic port

--- src/main.rs
+++ src/main.rs
@@ -144,7 +144,7 @@ async fn handle_connection(req: Request<Body>) -> Result<Response<Body>, hyper::
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr = ([127, 0, 0, 1], 0).into();
     let service =
         make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(handle_connection)) });
     let server = Server::bind(&addr).serve(service);
